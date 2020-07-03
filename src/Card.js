2c9cd7330d4cb2ea1f66b4319ffb19d239848911
Marco van Vemden <vanvemden@gmail.com>
2020-07-03 14:43:11 -0700

    add shuffle the deck functionality

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ image, value, suit }) {
 
   return (
-    <img src={image} alt={`${value} ${suit}`} />
+    <img src={image} alt={`${value} of ${suit}`} />
   )
 }
 
--- src/Deck.js
+++ src/Deck.js
@@ -8,42 +8,55 @@ const API_URL = 'https://deckofcardsapi.com/api/deck/';
 function Deck() {
   // state for the deckId
   const [deckId, setDeckId] = useState(null);
-  // state for cards [{image, value, suit}]
+  // state for cards [{image, value, suit, code, id}]
   const [cards, setCards] = useState([]);
+  // state for cards remaining
+  const [remaining, setRemaining] = useState(null);
+  // state for getting a new deck of cards
+  const [newDeck, setNewDeck] = useState(false);
 
-  // the effect should fetch the deck from API every time we mount // run out of cards
+  // the effect should fetch the deck from API every time we mount
+  // or request a new deck of cards 
   useEffect(function fetchDeckWhenMounted() {
     async function fetchDeck() {
       const resp = await axios.get(`${API_URL}/new/shuffle/?deck_count=1`);
-      console.log('response....', resp.data);
       setDeckId(resp.data.deck_id);
+      setRemaining(true);
     }
     fetchDeck();
-  }, []);
+
+    return function cleanUp() {
+      setCards([]);
+    };
+  }, [newDeck]);
 
   async function fetchNextCardFromDeck() {
     const resp = await axios.get(`${API_URL}${deckId}/draw/?count=1`);
-    console.log(resp);
+
     setCards((currCards) => {
       let card = resp.data.cards[0];
       card.id = uuid();
       return [card, ...currCards];
     });
+    if (resp.data.remaining === 0) {
+      setRemaining(false);
+    }
   }
 
   function handleNewCard() {
     fetchNextCardFromDeck();
   }
+  function handleErrorMsg() {
+    alert("Error: no cards remaining!");
+  }
 
   return (
     <div>
-      <button onClick={handleNewCard}>GIMMIE A CARD!</button>
+      <button onClick={remaining ? handleNewCard : handleErrorMsg}>GIMMIE A CARD!</button>
+      {!remaining ? <button onClick={() => setNewDeck(!newDeck)}>GIMMIE A NEW DECK!</button> : null}
       <div>{cards.map(card => <Card {...card} key={card.id} />)}</div>
     </div>
   );
 }
 
 export default Deck;
-
-// btn that clicks for new card
-// if remaining cards === 0, get a new deck
